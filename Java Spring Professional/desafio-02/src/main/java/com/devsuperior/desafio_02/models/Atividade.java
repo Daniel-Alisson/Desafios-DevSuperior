93a0b5502e1891ec1f0ef64f4126e661e62197eb
Daniel Alisson <130872709+Daniel-Alisson@users.noreply.github.com>
2025-02-12 12:21:35 -0300

    Criando a classe 'Atividade'
    Criando a classe Atividade e adicionei uma notação nos campos 'descricao' para ajustar a coluna do BD para TEXT

--- Java Spring Professional/desafio-02/src/main/java/com/devsuperior/desafio_02/models/Atividade.java
+++ Java Spring Professional/desafio-02/src/main/java/com/devsuperior/desafio_02/models/Atividade.java
@@ -3,21 +3,25 @@ package com.devsuperior.desafio_02.models;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
+@Table(name = "tb_atividade")
+public class Atividade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String nome;
     @Column(columnDefinition = "TEXT")
     private String descricao;
+    private Double preco;
 
-    public Categoria() {
+    public Atividade() {
     }
 
-    public Categoria(Integer id, String descricao) {
+    public Atividade(Integer id, String nome, String descricao, Double preco) {
         this.id = id;
+        this.nome = nome;
         this.descricao = descricao;
+        this.preco = preco;
     }
 
     public String getDescricao() {
@@ -34,5 +38,21 @@ public class Categoria {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
     }
 }
