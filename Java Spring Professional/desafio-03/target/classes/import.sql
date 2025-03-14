79d246cff60d848fde80882ef468ce98f1572f12
Daniel Alisson <130872709+Daniel-Alisson@users.noreply.github.com>
2025-03-14 11:32:13 -0300

    Base do Projeto

---
+++ Java Spring Professional/desafio-03/target/classes/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Daniel', '998.693.376-51', 800.00, '2001-05-15', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Alisson', '546.346.748-07', 1550.00, '2000-01-28', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Dayane', '685.676.296-83', 6200.00, '1995-09-15', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('David', '741.622.550-75', 7000.00, '2003-07-25', 5);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Mariana', '925.069.828-33', 9000.00, '1996-11-13', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Beatriz', '671.897.165-05', 500.00, '2005-04-06', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Lucas', '392.754.642-94', 2000.00, '1990-02-19', 4 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Vanessa ', '447.579.380-71', 12000.00, '2002-10-01', 8);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Camila', '040.244.869-34', 4000.00, '1999-03-03', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Thiago', '603.346.077-44', 10000.00, '2003-12-25', 8);
